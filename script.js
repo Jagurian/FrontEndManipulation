5cd5b1ccc1617b0c77a6724ce7985d03b993a595
Jagurian <antoinesoto19977@gmail.com>
2023-06-06 16:58:27 +0200

    js

---
+++ script.js
@@ -0,0 +1,96 @@
+// document.title = "Modifying the DOM"
+
+// console.log(document.title);
+
+// document.body.style.backgroundColor = "#FF69B4";
+
+// function getRandomColor() {
+//     const r = Math.floor(Math.random() * 256);
+//     const g = Math.floor(Math.random() * 256);
+//     const b = Math.floor(Math.random() * 256);
+//     return `rgb(${r}, ${g}, ${b})`;
+// }
+
+// function changeBackgroundColor() {
+//     const randomColor = getRandomColor();
+//     document.body.style.backgroundColor = randomColor;
+// }
+
+// setInterval(changeBackgroundColor, 1000);
+
+// const secondChild = document.body.children[1];
+
+// for (const child of secondChild.children) {
+//   console.log(child);
+// }
+
+
+// const importantElements = document.querySelectorAll(".important");
+
+// importantElements.forEach((element) => {
+//     element.setAttribute('title', 'This is an important item');
+//   });
+
+
+// const imgElements = document.querySelectorAll('img');
+
+// imgElements.forEach((img) => {
+//   if (!img.classList.contains('important')) {
+//     img.style.display = 'none';
+//   }
+// });
+
+// const paragraphs = document.querySelectorAll('p');
+
+// paragraphs.forEach((paragraph) => {
+//   const content = paragraph.textContent;
+//   const className = paragraph.className;
+
+//   console.log('Content:', content);
+
+//   if (className) {
+//     console.log('Class:', className);
+//   }
+// });
+
+
+// const paragraphs = document.querySelectorAll('p');
+
+// paragraphs.forEach((paragraph) => {
+//   const content = paragraph.textContent;
+//   const className = paragraph.className;
+
+//   console.log('Content:', content);
+
+//   if (className) {
+//     console.log('Class:', className);
+//   }
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
